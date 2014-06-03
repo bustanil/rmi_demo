d0792b9b9ba6854a5654683c9e500ebba6ca53be
Bustanil Arifin <bustanil.arifin@gmail.com>
2014-06-04 00:00:40 +0700

    migrated to spring boot

--- src/main/java/id/co/collega/AppClient.java
+++ src/main/java/id/co/collega/AppClient.java
@@ -1,12 +1,28 @@
 package id.co.collega;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
+@Configuration
+@EnableAutoConfiguration
 public class AppClient {
 
+    @Bean(name = "personService")
+    PersonService personService() {
+        RmiProxyFactoryBean rmiProxyFactoryBean = new RmiProxyFactoryBean();
+        rmiProxyFactoryBean.setServiceInterface(PersonService.class);
+        rmiProxyFactoryBean.setServiceUrl("rmi://localhost:1199/PersonService");
+        rmiProxyFactoryBean.afterPropertiesSet();
+        return (PersonService) rmiProxyFactoryBean.getObject();
+    }
+
     public static void main(String[] args) {
-        ApplicationContext client = new ClassPathXmlApplicationContext("client.xml");
+        ApplicationContext client = SpringApplication.run(AppClient.class, args);
         PersonService personService = client.getBean(PersonService.class);
         personService.createPerson("Bustanil");
     }
--- src/main/java/id/co/collega/AppServer.java
+++ src/main/java/id/co/collega/AppServer.java
@@ -1,16 +1,61 @@
 package id.co.collega;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.remoting.rmi.RmiServiceExporter;
 
-/**
- * Hello world!
- *
- */
-public class AppServer
-{
+import javax.sql.DataSource;
+
+@Configuration
+@EnableAutoConfiguration
+public class AppServer implements CommandLineRunner {
+
+    @Bean
+    DataSource dataSource() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/rmi_demo");
+        basicDataSource.setUsername("root");
+        basicDataSource.setPassword("");
+        return basicDataSource;
+    }
+
+    @Bean
+    JdbcTemplate jdbcTemplate(DataSource dataSource){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
+    }
+
+    @Bean
+    PersonService personService(JdbcTemplate jdbcTemplate){
+        PersonServiceImpl personService = new PersonServiceImpl();
+        personService.setJdbcTemplate(jdbcTemplate);
+        return personService;
+    }
+
+    @Bean
+    RmiServiceExporter rmiServiceExporter(PersonService personService){
+        RmiServiceExporter rmiServiceExporter = new RmiServiceExporter();
+        rmiServiceExporter.setServiceName("PersonService");
+        rmiServiceExporter.setServiceInterface(PersonService.class);
+        rmiServiceExporter.setService(personService);
+        rmiServiceExporter.setRegistryPort(1199);
+        return rmiServiceExporter;
+    }
+
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("server.xml");
+        SpringApplication.run(AppServer.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println("Hello world");
     }
 }
